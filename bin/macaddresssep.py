ec5039b46a8a921f8d714780990d52c579141342
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-23 18:30:22 +0100

    Created bin/template.py

---
+++ bin/macaddresssep.py
@@ -0,0 +1,19 @@
+"""macaddresssep
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-23"
+
+class macaddresssep:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(macaddresssep.macaddresssep())
+	
+	def macaddresssep():
+		"""macaddresssep first funtion
+		"""
+		return "macaddresssep" #for the test (see test_macaddresssep.py)
+		
+if __name__ == "__main__":
+	macaddresssep()
